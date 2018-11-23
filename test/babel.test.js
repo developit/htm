eda712bd926e5c09daa4a8a4dc5ce670888f333a
Joachim Viide <jviide@iki.fi>
2018-11-24 01:21:57 +0200

    Add a test case for valid code generation

--- test/babel.test.js
+++ test/babel.test.js
@@ -57,4 +57,16 @@ describe('htm/babel', () => {
 			}).code
 		).toBe(`var name="world",vnode={type:1,tag:"div",props:{id:"hello"},children:[{type:3,tag:null,props:null,children:null,text:"hello, "},name],text:null};`);
 	});
+
+	test('produce valid code for valid templates', () => {
+		expect(
+			transform('html`<div>",${name}</div>`;', {
+				babelrc: false,
+				compact: true,
+				plugins: [
+					htmBabelPlugin
+				]
+			}).code
+		).toBe(`h("div",{},["\\",",name]);`);
+	});
 });
