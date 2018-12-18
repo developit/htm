cc55c0a69dce82b53768deb3768760b8f4d19a6a
Jason Miller <developit@google.com>
2018-12-17 22:23:11 -0500

    Add performance test

---
+++ test/perf.test.mjs
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import htm from '../src/index.mjs';
+
+const h = (tag, props, ...children) => ({ tag, props, children });
+const html = htm.bind(h);
+
+describe('performance', () => {
+	test('creation', () => {
+		const results = [];
+		const Foo = ({ name }) => html`<div class="foo">${name}</div>`;
+		const statics = [
+			'\n<div id=app data-loading="true">\n\t<h1>Hello World</h1>\n\t<ul class="items" id=', '>\n\t',
+			'\n\t</ul>\n\t\n\t<', ' name="foo" />\n\t<', ' name="other">content</', '>\n\n</div>'
+		];
+		let count = 0;
+		function go(count) {
+			return html(
+				statics.concat(['count:', count]),
+				`id${count}`,
+				html(['<li data-id="','">', '</li>'], 'i'+count, 'some text #'+count),
+				Foo, Foo, Foo
+			);
+		}
+		let now = performance.now();
+		const start = now;
+		while ((now = performance.now()) < start+1000) {
+			count++;
+			if (results.push(String(go(count)))===10) results.length = 0;
+		}
+		const elapsed = now - start;
+		const hz = count / elapsed * 1000;
+		// eslint-disable-next-line no-console
+		console.log(`Creation: ${hz|0}/s, average: ${elapsed/count.toFixed(4)}ms`);
+		expect(elapsed).toBeGreaterThan(999);
+		expect(hz).toBeGreaterThan(1000);
+	});
+});
