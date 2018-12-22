2ca8b364da73d76111e58fa2779fab567c43c860
Nathan Graule <solarliner@gmail.com>
2018-12-22 18:38:22 +0100

    Add missing Component export from Preact integration

--- src/integrations/preact/index.d.ts
+++ src/integrations/preact/index.d.ts
@@ -1,4 +1,4 @@
-import { h, VNode } from 'preact';
+import { h, VNode, Component } from 'preact';
 declare function render(tree: VNode, parent: HTMLElement): void;
 declare const html: (strings: TemplateStringsArray, ...values: any[]) => VNode;
-export { h, html, render };
+export { h, html, render, Component };
