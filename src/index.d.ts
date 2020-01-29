5e00c2081a5da8ba377dcd0a7336858078cc6b2a
Yuki Hattori <yukihattori1116@gmail.com>
2020-01-29 21:20:48 +0900

    Fix typing: export the identified w/ declared type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,6 @@
-export default {
+declare const htm: {
   bind<HResult>(
     h: (type: any, props: Record<string, any>, ...children: any[]) => HResult
   ): (strings: TemplateStringsArray, ...values: any[]) => HResult | HResult[];
 };
+export default htm;
