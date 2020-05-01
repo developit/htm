f4bec0a68987826db1c24ae8e634a63242bb7e4a
Jason Miller <developit@users.noreply.github.com>
2020-05-01 10:40:03 -0400

    Add a couple basic assertions to the dist output

--- test/fixtures/esm/index.js
+++ test/fixtures/esm/index.js
@@ -1,5 +1,14 @@
-import "htm";
-import "htm/preact";
-import "htm/preact/standalone";
-// TODOD: Enable once react distro is ESM compatible.
-// import "htm/react";
+import assert from 'assert';
+import htm from 'htm';
+import * as preact from 'htm/preact';
+import * as standalone from 'htm/preact/standalone';
+// TODO: Enable once react distro is ESM compatible.
+// import * as react 'htm/react';
+
+assert(typeof htm === 'function', 'import htm from "htm"');
+
+assert(typeof preact.html === 'function', 'import { html } from "preact"');
+
+assert(typeof standalone.html === 'function', 'import { html } from "preact/standalone"');
+
+console.log('✅ Dist Tests Passed');
